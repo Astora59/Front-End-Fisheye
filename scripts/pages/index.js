8550ea44a2c8f1148bc6f86fefea20e02021fb8b
Yamin Moulay <yaminmoulay12@gmail.com>
2023-08-31 11:54:43 +0200

    eslint and prettier configured

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,24 @@
-    async function getPhotographers() {
-        const dataPhotographers = await fetch('/data/photographers.json');
-        const infoProfilePhotographers = await dataPhotographers.json();
-        console.log(infoProfilePhotographers);
-        
-        return infoProfilePhotographers;
+async function getPhotographers() {
+  const dataPhotographers = await fetch('/data/photographers.json')
+  const infoProfilePhotographers = await dataPhotographers.json()
+  return infoProfilePhotographers
+}
 
-        
-    }
+async function displayData(photographers) {
+  const photographersSection = document.querySelector('.photographer_section')
+  let tabIndex = 2
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer, tabIndex)
+    const userCardDOM = photographerModel.getUserCardDOM()
+    photographersSection.appendChild(userCardDOM)
+    tabIndex++
+  })
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-        let tabIndex = 2
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer, tabIndex);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-            tabIndex++
-        });
-    }
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers()
+  displayData(photographers)
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+init()
